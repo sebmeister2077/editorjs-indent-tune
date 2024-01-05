75ef744e57a127ae39e761e94fb6ca621521eab8
romanunr02 <romanunr02@gmail.com>
2024-01-05 17:15:47 +0200

    transformed js files into ts files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { LEFT_ARROW_ICON, RIGHT_ARROW_ICON } from './icons.js'
+import { type BlockTune, type API, type BlockAPI } from '@editorjs/editorjs'
+import { type TunesMenuConfig } from '@editorjs/editorjs/types/tools/index.js'
+import './index.css'
 
-export default class IndentTune {
+const WRAPPER_NAME = 'data-block-indent-wrapper'
+
+export default class IndentTune implements BlockTune {
     static get isTune() {
         return true
     }
 
+    private api: API
+    private block: BlockAPI
+    private config: Record<'indentSize' | 'maxIndent' | 'minIndent', number> & {
+        tuneName: string | null
+        multiblock: boolean
+        orientation: 'horizontal' | 'vertical'
+    }
+    public data: { indentLevel: number }
+    private wrapper: HTMLElement
     constructor({ api, data, config, block }) {
         this.api = api
         this.block = block
@@ -20,10 +34,7 @@ export default class IndentTune {
     // reset?(): void | Promise<void> {
     // }
 
-    /**
-     * @returns {HTMLElement | TunesMenuConfig}
-     */
-    render() {
+    public render(): HTMLElement | TunesMenuConfig {
         //Disable items after they are rendered synchronously
         setTimeout(() => {
             if (this.data.indentLevel == this.config.maxIndent) this.getTuneButton('indent')?.classList.add(this.CSS.disabledItem)
@@ -45,6 +56,30 @@ export default class IndentTune {
                     name: `${this.TuneNames.unindent}-${this.block.id}`,
                 },
             ]
+        // this.styles = document.createElement('style')
+        // ;(this.styles.innerHTML = /*css*/ `
+        //         .${this.CSS.customPopoverItem}:hover {
+        //             background-color: transparent !important;
+        //         }
+
+        //         .${this.CSS.customPopoverItem} .${this.CSS.popoverItemIcon} {
+        //             will-change: background-color;
+        //             transition: 0.3s background-color;
+        //             padding-block: 0px;
+        //             padding-inline: 0px;
+        //             border-width: 0px;
+        //         }
+
+        //         .${this.CSS.customPopoverItem}:hover .${this.CSS.popoverItemIcon} {
+        //             box-shadow: 0 0 0 1px var(--color-border-icon, rgba(201, 201, 204, .48)) !important;
+        //             -webkit-box-shadow: 0 0 0 1px var(--color-border-icon, rgba(201, 201, 204, .48)) !important;
+        //         }
+
+        //         .${this.CSS.customPopoverItem} .${this.CSS.popoverItemIcon}:hover {
+        //             background-color: var(--color-background-item-hover, #eff2f5);
+        //         }
+        // `),
+        //     document.body.appendChild(this.styles)
         const html = /*html*/ `
 			<div class="${this.CSS.popoverItem} ${this.CSS.customPopoverItem}" data-item-name='indent'>
 				<button class="${this.CSS.popoverItemIcon}" data-unindent>${LEFT_ARROW_ICON}</button>
@@ -53,7 +88,7 @@ export default class IndentTune {
 			</div>
 		`
 
-        const item = new DOMParser().parseFromString(html, 'text/html').body.firstChild
+        const item = new DOMParser().parseFromString(html, 'text/html').body.firstChild as HTMLElement
 
         item.querySelector('[data-indent]')?.addEventListener('click', () => this.indentBlock())
         item.querySelector('[data-unindent]')?.addEventListener('click', () => this.unIndentBlock())
@@ -61,11 +96,7 @@ export default class IndentTune {
         return item
     }
 
-    /**
-     * @param {HTMLElement} pluginsContent
-     * @returns {HTMLElement}
-     */
-    wrap(pluginsContent) {
+    public wrap(pluginsContent: HTMLElement): HTMLElement {
         this.wrapper = document.createElement('div')
         this.wrapper.appendChild(pluginsContent)
         this.wrapper.setAttribute(WRAPPER_NAME, '')
@@ -84,7 +115,12 @@ export default class IndentTune {
                 if (!this.config.multiblock || blocks.length < 2) {
                     if (isIndent) this.indentBlock()
                     else this.unIndentBlock()
-                    b.dispatchChange()
+                    this.block.dispatchChange()
+                    return
+                }
+
+                if (!Boolean(this.config.tuneName)) {
+                    console.error(`'tuneName' is empty.`)
                     return
                 }
 
@@ -94,9 +130,9 @@ export default class IndentTune {
                     if (!savedData) return
 
                     //this somehow SAVES fine
-                    const tune = savedData.tunes[this.config.tuneName]
+                    const tune = (savedData as any).tunes?.[this.config.tuneName!]
                     console.assert(Boolean(tune), `'tuneName' is invalid, no tune was found for block ${b.name}`)
-                    if (isIndent) tune.indentLevel = Math.min(MAX_INDENT, (tune.indentLevel ?? 0) + 1)
+                    if (isIndent) tune.indentLevel = Math.min(this.config.maxIndent, (tune.indentLevel ?? 0) + 1)
                     else tune.indentLevel = Math.max(0, (tune.indentLevel ?? 0) - 1)
                     b.dispatchChange()
 
@@ -113,9 +149,13 @@ export default class IndentTune {
         return this.wrapper
     }
 
-    get CSS() {
+    public save() {
+        return this.data
+    }
+
+    private get CSS() {
         return {
-            cusotmPopoverItem: ' ce-popover-item-custom',
+            customPopoverItem: ' ce-popover-item-custom',
             popoverItem: 'ce-popover-item',
             popoverItemIcon: 'ce-popover-item__icon',
             popoverItemTitle: 'ce-popover-item__title',
@@ -123,77 +163,58 @@ export default class IndentTune {
         }
     }
 
-    get TuneNames() {
+    private get TuneNames() {
         return {
             indent: 'tune-indent',
             unindent: 'tune-unindent',
         }
     }
 
-    indentBlock() {
+    private indentBlock() {
         if (!this.wrapper) return
         this.data.indentLevel = Math.min(this.data.indentLevel + 1, this.config.maxIndent)
 
-        this.applyStylesToWrapper(this.wrapper)
+        this.applyStylesToWrapper(this.wrapper, this.data.indentLevel)
 
         //disable tune
         this.getTuneButton('unindent')?.classList.remove(this.CSS.disabledItem)
         if (this.data.indentLevel == this.config.maxIndent) this.getTuneButton('indent')?.classList.add(this.CSS.disabledItem)
     }
 
-    unIndentBlock() {
+    private unIndentBlock() {
         if (!this.wrapper) return
         this.data.indentLevel = Math.max(this.data.indentLevel - 1, 0)
 
-        this.applyStylesToWrapper(this.wrapper)
+        this.applyStylesToWrapper(this.wrapper, this.data.indentLevel)
 
         // disable tune
         this.getTuneButton('indent')?.classList.remove(this.CSS.disabledItem)
         if (this.data.indentLevel == 0) this.getTuneButton('unindent')?.classList.add(this.CSS.disabledItem)
     }
 
-    getTuneButton(indentType) {
+    private getTuneButton(indentType: 'indent' | 'unindent') {
         return this.config.orientation === 'vertical'
             ? this.getTuneByName(`${this.TuneNames[indentType]}[data-item-name=${this.block.id}]`)
             : document.querySelector(`.${this.CSS.popoverItemIcon}[data-${indentType}]`)
     }
-    /**
-     * @param {string} name
-     * @returns {HTMLElement}
-     */
-    getTuneByName(name) {
+
+    private getTuneByName(name: string) {
         return document.querySelector(`.${this.CSS.popoverItem}[data-item-name=${name}]`)
     }
 
-    /**
-     *
-     * @param {HTMLElement} wrapper
-     * @param {number} indentLevel
-     */
-    applyStylesToWrapper(wrapper, indentLevel) {
+    private applyStylesToWrapper(wrapper: HTMLElement, indentLevel: number) {
         wrapper.style.paddingLeft = `${indentLevel * this.config.indentSize}px`
     }
 
-    /**
-     * @returns {BlockAPI[]}
-     */
-    getGlobalSelectedBlocks() {
+    private getGlobalSelectedBlocks() {
         const crossSelectedBlocks = new Array(this.api.blocks.getBlocksCount())
             .fill(0)
             .map((_, idx) => this.api.blocks.getBlockByIndex(idx))
-            .filter((b) => !!b?.selected)
+            .filter((b): b is BlockAPI => !!b?.selected)
         return crossSelectedBlocks
     }
 
-    /**
-     * @param {string} blockId
-     * @returns {HTMLElement | null}
-     */
-    getWrapperBlockById(blockId) {
+    private getWrapperBlockById(blockId: string) {
         return document.querySelector(`.ce-block[data-id="${blockId}"] [${WRAPPER_NAME}]`)
-    }
-
-    save() {
-        return this.data
     }
 }
