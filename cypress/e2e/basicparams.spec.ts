1f75f60984c3b857d2c202ca689177be6b88cb24
romanunr02 <romanunr02@gmail.com>
2025-02-16 20:17:16 +0200

    added some code for the tests

--- cypress/e2e/basicparams.spec.ts
+++ cypress/e2e/basicparams.spec.ts
@@ -1,7 +1,36 @@
+import { EDITOR_VERSIONS, TEMP_ENVIRONMENT_URL } from "../support/constants"
+import editorData from '../fixtures/data.json'
+
+
+
+
+
+
 // size, min max
 describe("Test editor basic params", () => {
 
-    it("It works", () => {
+    beforeEach(() => {
+        cy.visit(TEMP_ENVIRONMENT_URL, {
+            onBeforeLoad(win) {
+                win.localStorage.setItem('editorjs-data-testing', JSON.stringify(editorData))
+            },
+        })
+
+        cy.applyBiggerGlobalFontSize();
 
     })
+
+    for (let i = 0; i < EDITOR_VERSIONS.length; i++) {
+        const version = EDITOR_VERSIONS[i];
+
+
+        context(`Verify version ${version}`, () => {
+
+
+            it("It works", () => {
+
+            })
+
+        })
+    }
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,27 @@
 
 
 // Cypress.Commands.add()
+Cypress.Commands.add('applyBiggerGlobalFontSize', () => {
+    cy.get("head").then(head => {
+        head[0].insertAdjacentHTML("beforeend", /*html*/`<style>*{
+                font-size:1.5rem !important;
+            }</style>`)
+    })
+})
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            // applyEditorSelection(startIndex: number, endIndex: number, version: EditorVersions[keyof EditorVersions]): Cypress.Chainable<void>
+            // applyUnderline(): Cypress.Chainable<void>
+            applyBiggerGlobalFontSize(): Cypress.Chainable<void>
+        }
+    }
+    // interface Window {
+    //     isEditorReady: Promise<void>;
+    //     editorVersion: string;
+    // }
+}
 
 
 export { }
